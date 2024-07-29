dce15289f17d94691f223c272e2ede64c1da4b9c
ibaesuyeon <ibaesuyeon@Naver.com>
2024-07-29 10:17:17 +0900

    [CHORE] 프로젝트 초기 설정
    스프링부트 초기 설정
    초기 라이브러리 : Lombok, Spring Web, Spring Boot DevTools, Data JPA, MYSQL Driver

---
+++ src/main/java/depth/mvp/neverland/NeverlandApplication.java
@@ -0,0 +1,13 @@
+package depth.mvp.neverland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeverlandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeverlandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/depth/mvp/neverland/NeverlandApplicationTests.java
@@ -0,0 +1,13 @@
+package depth.mvp.neverland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeverlandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
