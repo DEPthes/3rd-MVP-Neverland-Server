ac841fe55637ee5099b6789f38383c306ecdfdb0
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-08-04 18:57:58 +0900

    [FEATURE] 학사일정 entity dto mapper 추가

--- src/main/java/depth/mvp/thinkerbell/domain/notice/service/AcademicScheduleMapper.java
+++ src/main/java/depth/mvp/thinkerbell/domain/notice/service/AcademicScheduleMapper.java
@@ -3,11 +3,17 @@ package depth.mvp.thinkerbell.domain.notice.service;
 import depth.mvp.thinkerbell.domain.common.entity.Univ;
 import depth.mvp.thinkerbell.domain.notice.dto.AcademicScheduleDto;
 import depth.mvp.thinkerbell.domain.notice.entity.AcademicSchedule;
+import depth.mvp.thinkerbell.global.exception.ErrorCode;
+import depth.mvp.thinkerbell.global.exception.MapperException;
 
 public class AcademicScheduleMapper {
 
     //entity to dto
     public static AcademicScheduleDto toDto(AcademicSchedule academicSchedule) {
+        if (academicSchedule == null) {
+            throw new MapperException(ErrorCode.INVALID_INPUT_VALUE);
+        }
+
         return AcademicScheduleDto.builder()
                 .id(academicSchedule.getId())
                 .title(academicSchedule.getTitle())
@@ -18,6 +24,10 @@ public class AcademicScheduleMapper {
 
     //dto to entity
     public static AcademicSchedule toEntity(AcademicScheduleDto academicScheduleDto, Univ univ) {
+        if (academicScheduleDto == null || univ == null) {
+            throw new MapperException(ErrorCode.INVALID_INPUT_VALUE);
+        }
+
         return AcademicSchedule.builder()
                 .title(academicScheduleDto.getSchedule())
                 .schedule(academicScheduleDto.getSchedule())
