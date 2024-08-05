c78617907a94fdf41a913b45d3283cca98c9da3a
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-08-05 18:48:52 +0900

    [FEATURE] 학사일정 dto 추가

--- src/main/java/depth/mvp/thinkerbell/domain/notice/dto/AcademicScheduleDto.java
+++ src/main/java/depth/mvp/thinkerbell/domain/notice/dto/AcademicScheduleDto.java
@@ -12,11 +12,4 @@ public class AcademicScheduleDto {
     private String schedule;
     private Long univId;
 
-    @Builder
-    public AcademicScheduleDto(Long id, String title, String schedule, Long univId) {
-        this.id = id;
-        this.title = title;
-        this.schedule = schedule;
-        this.univId = univId;
-    }
 }
