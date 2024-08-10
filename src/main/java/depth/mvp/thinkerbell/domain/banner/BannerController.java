90e66472c20a99bb9a29ca87aed4e71dd15c062e
minko <minye0812@naver.com>
2024-08-10 21:17:09 +0900

    [FEAT] 배너 조회 기능 swagger 추가

--- src/main/java/depth/mvp/thinkerbell/domain/banner/BannerController.java
+++ src/main/java/depth/mvp/thinkerbell/domain/banner/BannerController.java
@@ -1,7 +1,12 @@
 package depth.mvp.thinkerbell.domain.banner;
 
+
+import depth.mvp.thinkerbell.global.dto.ApiResult;
+import depth.mvp.thinkerbell.global.exception.ErrorCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,9 +20,19 @@ public class BannerController {
 
     private final BannerService bannerService;
 
+    @Operation(summary = "배너 이미지 조회", description = "배너 이미지를 조회합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 조회됨"),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력 값"),
+            @ApiResponse(responseCode = "500", description = "서버 오류 발생")
+    })
     @GetMapping
-    public ResponseEntity<List<BannerDTO>> getAllBanners() {
-        List<BannerDTO> banners = bannerService.getAllBanners();
-        return ResponseEntity.ok(banners);
+    public ApiResult<List<BannerDTO>> getAllBanners() {
+        try {
+            List<BannerDTO> banners = bannerService.getAllBanners();
+            return ApiResult.ok(banners);
+        } catch (RuntimeException e) {
+            return ApiResult.withError(ErrorCode.INTERNAL_SERVER_ERROR, null);
+        }
     }
 }
