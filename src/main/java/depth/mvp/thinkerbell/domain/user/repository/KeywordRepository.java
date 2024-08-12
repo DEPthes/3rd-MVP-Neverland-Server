d072cb7c5187042f5a022d722771ed4631aaa4b4
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-08-12 20:26:33 +0900

    [FEATURE] 키워드 Repository 추가

---
+++ src/main/java/depth/mvp/thinkerbell/domain/user/repository/KeywordRepository.java
@@ -0,0 +1,22 @@
+package depth.mvp.thinkerbell.domain.user.repository;
+
+import depth.mvp.thinkerbell.domain.user.entity.Keyword;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Keyword k WHERE k.keyword = :keyword AND k.user.id = :userId")
+    void deleteByKeywordAndUserId(@Param("keyword") String keyword, @Param("userId") Long userId);
+
+    boolean existsByKeywordAndUserId(String keyword, Long userId);
+
+    List<Keyword> findAllByUserId(Long userId);
+}
