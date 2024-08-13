e4874eede8432ce2a043ce4698b189010503ca9b
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-08-14 00:14:21 +0900

    [FEATURE] 필요 쿼리 추가

--- src/main/java/depth/mvp/thinkerbell/domain/notice/repository/AllNoticeViewRepository.java
+++ src/main/java/depth/mvp/thinkerbell/domain/notice/repository/AllNoticeViewRepository.java
@@ -10,4 +10,10 @@ import java.util.List;
 public interface AllNoticeViewRepository extends JpaRepository<AllNoticesView, Long> {
     @Query(value = "SELECT * FROM All_Notices_View e WHERE REPLACE(e.title, ' ', '') LIKE %:keyword%", nativeQuery = true)
     List<AllNoticesView> findByTitleContainingKeyword(@Param("keyword") String keyword);
+
+    @Query(value = "SELECT * FROM All_Notices_View WHERE table_name = :category AND id > :maxId", nativeQuery = true)
+    List<AllNoticesView> findNewNoticesByCategory(@Param("category") String category, @Param("maxId") Long maxId);
+
+    @Query(value = "SELECT MAX(id) FROM All_Notices_View WHERE table_name = :category", nativeQuery = true)
+    Long findMaxIdByCategory(@Param("category") String category);
 }
