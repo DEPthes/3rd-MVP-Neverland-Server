b9bf9bd1ec02a0177d4f909a14bd4db60c3fd082
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-08-14 00:24:20 +0900

    [FEATURE] 알람 엔티티 칼럼 추가

--- src/main/java/depth/mvp/thinkerbell/domain/user/entity/Alarm.java
+++ src/main/java/depth/mvp/thinkerbell/domain/user/entity/Alarm.java
@@ -1,14 +1,12 @@
 package depth.mvp.thinkerbell.domain.user.entity;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Getter
+@Setter
 @Table(name = "Alarm")
 public class Alarm {
 
@@ -17,15 +15,20 @@ public class Alarm {
     private Long id;
     private Long noticeID;
     private String noticeType;
+    private String title;
+    private String keyword;
+    private Boolean isViewed = false;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
     @Builder
-    public Alarm(Long noticeID, String noticeType, User user) {
+    public Alarm(Long noticeID, String noticeType, User user, String title, String keyword) {
         this.noticeID = noticeID;
         this.noticeType = noticeType;
         this.user = user;
+        this.title = title;
+        this.keyword = keyword;
     }
 }
