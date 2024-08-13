cb66549a37fb53e584749334fdf43df40f7471b9
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-08-14 00:22:59 +0900

    [CHORE] FCM 설정

---
+++ src/main/java/depth/mvp/thinkerbell/global/config/FirebaseConfig.java
@@ -0,0 +1,25 @@
+package depth.mvp.thinkerbell.global.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase/neverland-thinkerbell-firebase-adminsdk-n4ik2-49baee872c.json");
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        return FirebaseApp.initializeApp(options);
+    }
+}
