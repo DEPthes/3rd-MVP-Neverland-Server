74cb756fb288d10e48bb43bc9f46d9bd22292096
ibaesuyeon <ibaesuyeon@Naver.com>
2024-08-15 20:46:15 +0900

    [FEAT] 명지대 대학안전공지 조회 구현

---
+++ src/main/java/depth/mvp/thinkerbell/domain/notice/controller/SafetyNoticeController.java
@@ -0,0 +1,40 @@
+package depth.mvp.thinkerbell.domain.notice.controller;
+
+import depth.mvp.thinkerbell.domain.notice.dto.SafetyNoticeDTO;
+import depth.mvp.thinkerbell.domain.notice.service.SafetyNoticeService;
+import depth.mvp.thinkerbell.global.dto.ApiResult;
+import depth.mvp.thinkerbell.global.exception.ErrorCode;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/safety")
+@RequiredArgsConstructor
+public class SafetyNoticeController {
+
+    private final SafetyNoticeService safetyNoticeService;
+
+    @Operation(summary = "명지대 대학안전 공지사항 조회", description = "명지대 대학안전 공지사항을 조회합니다. (https://www.mju.ac" +
+            ".kr/mjukr/8972/subview.do)")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 조회됨"),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력 값"),
+            @ApiResponse(responseCode = "500", description = "서버 오류 발생")
+    })
+    @GetMapping
+    public ApiResult<List<SafetyNoticeDTO>> getAllSafetyNotices() {
+        try {
+            List<SafetyNoticeDTO> notices = safetyNoticeService.getAllSafetyNotices();
+            return ApiResult.ok(notices);
+        } catch (RuntimeException e) {
+            return ApiResult.withError(ErrorCode.INTERNAL_SERVER_ERROR, null);
+        }
+    }
+}
