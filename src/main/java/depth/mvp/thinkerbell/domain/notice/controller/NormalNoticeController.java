9affdea1c216bc9e1ea793eaae073870231d8b27
ibaesuyeon <ibaesuyeon@Naver.com>
2024-08-16 11:30:27 +0900

    [FEAT] 명지대 일반공지 즐겨찾기 반환값 추가
    SSAID를 유저 식별자로 받아, 공지사항 조회 시 즐겨찾기 여부를 함께 반환

--- src/main/java/depth/mvp/thinkerbell/domain/notice/controller/NormalNoticeController.java
+++ src/main/java/depth/mvp/thinkerbell/domain/notice/controller/NormalNoticeController.java
@@ -8,10 +8,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,16 +20,18 @@ public class NormalNoticeController {
     private final NormalNoticeService normalNoticeService;
 
     @Operation(summary = "명지대 일반 공지사항 조회", description = "명지대 일반 공지사항을 조회합니다. (https://www.mju.ac" +
-            ".kr/mjukr/255/subview.do) 중요 공지를 조회할 땐 인자로 true, 그 외 공지를 조회할 땐 false를 전달해주세요.")
+            ".kr/mjukr/255/subview.do) 중요 공지를 조회할 땐 인자(important)로 true, 그 외 공지를 조회할 땐 false를 전달해주세요.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공적으로 조회됨"),
             @ApiResponse(responseCode = "400", description = "잘못된 입력 값"),
             @ApiResponse(responseCode = "500", description = "서버 오류 발생")
     })
-    @GetMapping("/{important}")
-    public ApiResult<List<NormalNoticeDTO>> getAllNormalNotices(@PathVariable("important") boolean important) {
+    @GetMapping
+    public ApiResult<List<NormalNoticeDTO>> getAllNormalNotices(@RequestParam(value = "important", required = false,
+                                                                              defaultValue = "false") boolean important,
+                                                                @RequestParam("ssaid") String ssaid) {
         try {
-            List<NormalNoticeDTO> notices = normalNoticeService.getAllNormalNotices(important);
+            List<NormalNoticeDTO> notices = normalNoticeService.getAllNormalNotices(important, ssaid);
             return ApiResult.ok(notices);
         } catch (RuntimeException e) {
             return ApiResult.withError(ErrorCode.INTERNAL_SERVER_ERROR, null);
