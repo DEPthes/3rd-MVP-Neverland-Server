14ffc4c671ebe8124f464de556450710671baf2e
ibaesuyeon <ibaesuyeon@Naver.com>
2024-08-17 09:05:09 +0900

    [FEAT] 도서관 공지사항 즐겨찾기 반환값 추가
    DTO 빌더 패턴 적용

--- src/main/java/depth/mvp/thinkerbell/domain/notice/repository/TeachingNoticeRepository.java
+++ src/main/java/depth/mvp/thinkerbell/domain/notice/repository/TeachingNoticeRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeachingNoticeRepository extends JpaRepository<TeachingNotice, Long> {
-    Page<TeachingNotice> findAllByOrderByIsImportantDescPubDateDesc(Pageable pageable);
+    Page<TeachingNotice> findAllByOrderByImportantDescPubDateDesc(Pageable pageable);
 }
