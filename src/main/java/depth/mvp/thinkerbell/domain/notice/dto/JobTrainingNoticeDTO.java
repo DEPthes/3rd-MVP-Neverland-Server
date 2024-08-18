0001436b139f0197430109fec87349d9e6e76697
ibaesuyeon <ibaesuyeon@Naver.com>
2024-08-18 13:55:08 +0900

    [FIX] 현장실습 반환값 id 추가

--- src/main/java/depth/mvp/thinkerbell/domain/notice/dto/JobTrainingNoticeDTO.java
+++ src/main/java/depth/mvp/thinkerbell/domain/notice/dto/JobTrainingNoticeDTO.java
@@ -1,6 +1,5 @@
 package depth.mvp.thinkerbell.domain.notice.dto;
 
-import depth.mvp.thinkerbell.domain.notice.entity.JobTrainingNotice;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,6 +8,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class JobTrainingNoticeDTO {
+    private Long id;
     private String company;
     private String year;
     private String semester;
@@ -19,17 +19,4 @@ public class JobTrainingNoticeDTO {
     private String jobName;
     private boolean marked;
 
-    // 엔티티를 DTO로 변환하는 정적 메서드
-//    public static JobTrainingNoticeDTO fromEntity(JobTrainingNotice notice) {
-//        return new JobTrainingNoticeDTO(
-//                notice.getCompany(),
-//                notice.getYear(),
-//                notice.getSemester(),
-//                notice.getPeriod(),
-//                notice.getMajor(),
-//                notice.getRecrutingNum(),
-//                notice.getDeadline(),
-//                notice.getJobName()
-//        );
-//    }
 }
