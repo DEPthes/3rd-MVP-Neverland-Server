44cd2959458f38b79947bbb07df54799c2cb3eef
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-08-19 23:57:08 +0900

    [FEATURE] 알림 테스트 기능 구현

--- src/main/java/depth/mvp/thinkerbell/TestController.java
+++ src/main/java/depth/mvp/thinkerbell/TestController.java
@@ -1,5 +1,6 @@
 package depth.mvp.thinkerbell;
 
+import depth.mvp.thinkerbell.domain.user.service.AlarmService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,9 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin("*")
 public class TestController {
 
+    private final AlarmService alarmService;
+
     @GetMapping("/test")
     public String test(){
         return "테스트";
     }
 
+    @GetMapping("/alarm-test")
+    public void testAlarm(){
+        alarmService.updateNoticeAndMatchKeywordTest();
+    }
 }
