9a8961da932d5d6c587422193a62ae238ffdcfbd
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-08-21 06:21:04 +0900

    [FEATURE] 카테고리 CamelCase화 기능 추가

--- src/main/java/depth/mvp/thinkerbell/domain/common/service/CategoryService.java
+++ src/main/java/depth/mvp/thinkerbell/domain/common/service/CategoryService.java
@@ -12,23 +12,38 @@ import java.util.Map;
 @Service
 public class CategoryService {
 
-    private Map<String, String> categoryMap;
+    private Map<String, String> categoryMap1;
+    private Map<String, String> categoryMap2;
 
     public CategoryService() {
         loadCategoryMap();
+        upperCategoryMap();
     }
 
     private void loadCategoryMap() {
         ObjectMapper objectMapper = new ObjectMapper();
 
         try(InputStream inputStream = new ClassPathResource("Categories.json").getInputStream()) {
-            categoryMap = objectMapper.readValue(inputStream, new TypeReference<Map<String, String>>() {});
+            categoryMap1 = objectMapper.readValue(inputStream, new TypeReference<Map<String, String>>() {});
+        } catch (IOException e) {
+            throw new RuntimeException("카테고리 메핑 파일을 로드하는 동안 오류가 발생했습니다.",e);
+        }
+    }
+
+    private void upperCategoryMap() {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try(InputStream inputStream = new ClassPathResource("Category.json").getInputStream()) {
+            categoryMap2 = objectMapper.readValue(inputStream, new TypeReference<Map<String, String>>() {});
         } catch (IOException e) {
             throw new RuntimeException("카테고리 메핑 파일을 로드하는 동안 오류가 발생했습니다.",e);
         }
     }
 
     public String getCategoryNameInKorean(String category) {
-        return categoryMap.getOrDefault(category, category);
+        return categoryMap1.getOrDefault(category, category);
+    }
+    public String getCategoryUpper(String category) {
+        return categoryMap2.getOrDefault(category, category);
     }
 }
