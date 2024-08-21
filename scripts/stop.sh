95c4292a795cf35eb2f29ca5a90977150ce67684
BaeSuYeon <71921747+ibaesuyeon@users.noreply.github.com>
2024-08-22 06:05:25 +0900

    Update stop.sh

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -14,6 +14,12 @@ CURRENT_PID=$(pgrep -f $JAR_FILE)
 if [ -z $CURRENT_PID ]; then
   echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
 else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료" >> $DEPLOY_LOG
   kill -15 $CURRENT_PID
+  sleep 10  # 종료 후 잠시 기다려 포트가 해제되도록 함
+  # 프로세스가 아직 종료되지 않았을 경우 강제 종료
+  CURRENT_PID=$(pgrep -f "java -jar $PROJECT_ROOT/$JAR_FILE")
+  if [ ! -z "$CURRENT_PID" ]; then
+    kill -9 $CURRENT_PID
+  fi
 fi
