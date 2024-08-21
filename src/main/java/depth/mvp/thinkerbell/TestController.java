ba235b5b2d4ad9f3434af10a379029b4fe504002
BaeSuYeon <71921747+ibaesuyeon@users.noreply.github.com>
2024-08-22 03:00:09 +0900

    Update TestController.java

--- src/main/java/depth/mvp/thinkerbell/TestController.java
+++ src/main/java/depth/mvp/thinkerbell/TestController.java
@@ -17,7 +17,7 @@ public class TestController {
 
     @GetMapping("/test")
     public String test(){
-        return "배포 CD 테스트";
+        return "CI/CD 테스트";
     }
 
     @GetMapping("/alarm-test")
